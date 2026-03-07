2943f86e8eea3bb3d27a597fb5d312365b68af77
Ananya Jain <ananyaj1515@gmail.com>
2026-03-08 00:24:41 +0800

    Implement tag parser

--- src/main/java/seedu/address/logic/commands/TagCommand.java
+++ src/main/java/seedu/address/logic/commands/TagCommand.java
@@ -1,11 +1,20 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG_GENDER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG_MAJOR;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG_YEAR;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 /**
@@ -17,7 +26,7 @@ public class TagCommand extends Command {
 
     public static final String COMMAND_WORD = "tag";
 
-    public static final String MESSAGE_USGAE = COMMAND_WORD + ":Adds a tag to the resident in the hall ledger"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":Adds a tag to the resident in the hall ledger"
             + "by the index number used in the displayed resident list."
             + "Existing tags will be overwritten by the input tags.\n"
             + "Parameters: INDEX (must be a positive integer)"
@@ -31,13 +40,46 @@ public class TagCommand extends Command {
     public static final String TAG_SUCCESS = "Added Tag to Resident: %1$s";
     public static final String TAG_NOT_ADDED = "At least one tag (year / major / gender) must be provided.";
 
+    public final Index index;
+    public final Set<Tag> tags;
+
+    public TagCommand(Index index, Set<Tag> tags) {
+        requireNonNull(index);
+        requireNonNull(tags);
+        this.index = index;
+        this.tags = tags;
+    }
+
     /**
      * Executes the tag command.
      *
      * @param model {@code Model} which the command should operate on.
      * @return a {@code CommandResult} describing the result of the command execution.
      */
-    public CommandResult execute(Model model) {
-        return new CommandResult("Tag added to resident in hall ledger");
+    public CommandResult execute(Model model) throws CommandException {
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToTag = lastShownList.get(index.getZeroBased());
+        Set<Tag> updatedTags = new HashSet<>(personToTag.getTags());
+        updatedTags.addAll(tags);
+
+        Person taggedPerson = new Person(
+                personToTag.getName(),
+                personToTag.getPhone(),
+                personToTag.getEmail(),
+                personToTag.getStudentId(),
+                personToTag.getRoomNumber(),
+                personToTag.getEmergencyContact(),
+                updatedTags
+        );
+
+        model.setPerson(personToTag, taggedPerson);
+        model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(String.format(TAG_SUCCESS, taggedPerson));
     }
 }
