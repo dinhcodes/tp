3d045fc18ce86b27eef39e7c75cc36a2aa6fa6a0
Pavithra <pavithra.achar1505@gmail.com>
2026-03-17 22:23:48 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -17,7 +17,6 @@ import seedu.address.model.person.EmergencyContact;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.RoomNumber;
-import seedu.address.model.person.RoomNumberTest;
 import seedu.address.model.person.StudentId;
 
 public class JsonAdaptedPersonTest {
